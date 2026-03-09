31518d03c116811999073c1cd801a3ce4ec0a1ac
Alexander Ashurov <bstuashurov@gmail.com>
2026-03-08 22:47:27 -0600

    Update fonts: Barlow Condensed for headings, Barlow for body

--- app/layout.jsx
+++ app/layout.jsx
@@ -20,7 +20,7 @@ export default function RootLayout({ children }) {
             <head>
                 <link rel="preconnect" href="https://fonts.googleapis.com" />
                 <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-                <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800;900&display=swap" rel="stylesheet" />
+                <link href="https://fonts.googleapis.com/css2?family=Barlow+Condensed:ital,wght@0,400;0,600;0,700;0,800;0,900;1,700;1,800&family=Barlow:wght@400;500;600;700;800&display=swap" rel="stylesheet" />
             </head>
             <body>
                 <Header />
